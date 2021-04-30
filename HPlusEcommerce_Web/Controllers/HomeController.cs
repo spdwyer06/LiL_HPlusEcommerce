2e3f25a26e1cf2527098a760baeaa417c2b05703
Sean Dwyer <spdwyer06@gmail.com>
2021-04-30 15:08:16 -0400

    Add Crawler filter

--- HPlusEcommerce_Web/Controllers/HomeController.cs
+++ HPlusEcommerce_Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HPlusEcommerce_Web.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace HPlusEcommerce_Web.Controllers
 {
+    [CrawlerFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ HPlusEcommerce_Web/Filters/CrawlerFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPlusEcommerce_Web.Filters
+{
+    public class CrawlerFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Browser.Crawler)
+            {
+                filterContext.Result = new HttpNotFoundResult();
+            }
+        }
+    }
+}
