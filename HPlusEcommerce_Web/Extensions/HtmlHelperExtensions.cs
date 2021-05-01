6ecf3ffa514c69c5995a90bb1bb366bac0082d0d
Sean Dwyer <spdwyer06@gmail.com>
2021-05-01 09:58:28 -0400

    Add Cart controller and Index view. Add shared layout displaying no footer

---
+++ HPlusEcommerce_Web/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Notice the manual namespace change
+namespace System.Web.Mvc
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString Copyright(this HtmlHelper helper)
+        {
+            return helper.Raw($"&copy; H + Sport {DateTime.Now.Year}");
+        }
+    }
+}
