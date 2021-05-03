464425b69ee737ce383d6fc953d71f75f8fecb11
Sean Dwyer <spdwyer06@gmail.com>
2021-05-03 13:52:42 -0400

    Update Account Index action. Add attributes to Login model

--- HPlusEcommerce_Web/Controllers/AccountController.cs
+++ HPlusEcommerce_Web/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HPlusEcommerce_Web.Data;
+using HPlusEcommerce_Web.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,16 @@ namespace HPlusEcommerce_Web.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            Customer customer = null;
+
+            using (var db = new ApplicationDbContext())
+            {
+                var user = db.Users.FirstOrDefault(u => u.EmailAddress == User.Identity.Name);
+
+                if (user != null)
+                    customer = db.Customers.FirstOrDefault(c => c.Id == user.CustomerId);
+            }
+
             return View();
         }
     }
--- HPlusEcommerce_Web/Models/Login.cs
+++ HPlusEcommerce_Web/Models/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,12 @@ namespace HPlusEcommerce_Web.Models
 {
     public class Login
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter a valid email address"]
         public string Username { get; set; }
 
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         public string Password { get; set; }
     }
 }
